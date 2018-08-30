6d1b9fe130f1e4b42ffa0d3b953b2c2b4e768207
Fedor Glazov <fedorglazov@gmail.com>
2018-08-30 16:36:34 +0200

    Add some lines which always exsist in a projective plane (up to isomorphism)

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,11 @@ use std::collections::HashSet;
 use std::hash::Hash;
 use std::hash::Hasher;
 use std::iter::FromIterator;
+use std::borrow::Cow;
 
 fn main() {
     println!("Hello, world!");
-    projective_plane_find(2);
+    projective_plane_find(3);
     println!("Done world!")
 }
 
@@ -18,17 +19,17 @@ struct Point {
 
 // A line in a projective plane, or in a struct which is being built to a projective plane
 #[derive(Eq, PartialEq, Debug, Clone)]
-struct Line<'a> {
-    points: HashSet<&'a Point>
+struct Line<'plane> {
+    points: HashSet<&'plane Point>
 }
 
-impl<'a> Line<'a> {
-    fn has_point(&'a self, point: &Point) -> bool {
+impl<'plane> Line<'plane> {
+    fn has_point(&'plane self, point: &Point) -> bool {
         self.points.contains(&point)
     }
 }
 
-impl<'a> Hash for Line<'a> {
+impl<'plane> Hash for Line<'plane> {
     fn hash<H: Hasher>(&self, state: &mut H) {
         // TODO: Maybe improve - might collide too often.
         let mut sum = 0;
@@ -40,20 +41,20 @@ impl<'a> Hash for Line<'a> {
 }
 
 // A candidate for a projective plane.
-struct Plane<'a> {
-    lines: HashSet<Line<'a>>,
+struct Plane<'plane> {
+    lines: HashSet<Line<'plane>>,
     points: HashSet<Point>,
     order: u32,
 }
 
-impl<'a> Plane<'a> {
+impl<'plane> Plane<'plane> {
     // During the entire procedure we wish to preserve the property that there is at most
     // one line connecting any two points.
     //
     // It is not hard to show that, if we have a set system where every line has n + 1 points
     // with n^2 + n + 1 elements and n^2 + n + 1 lines such that any two points have at
     // most one line containing them, then that system is a finite projective plane of order n.
-    fn find_all_line_candidates(&'a self) -> HashSet<Line> {
+    fn find_all_line_candidates(&'plane self) -> HashSet<Line> {
         let mut result = HashSet::new();
         for point in &self.points {
             let mut points_in_line = Vec::with_capacity((self.order + 1) as usize);
@@ -90,10 +91,8 @@ impl<'a> Plane<'a> {
 }
 
 fn find_line_candidates<'plane>(plane: &'plane Plane, points_in_line: Vec<&'plane Point>, allowed_points: HashSet<&'plane Point>) -> HashSet<Line<'plane>> {
-    let mut allowed_points = allowed_points;
-
     if points_in_line.len() == (plane.order + 1) as usize {
-        let mut set_of_points: HashSet<&'plane Point> = HashSet::from_iter(points_in_line);
+        let set_of_points: HashSet<&'plane Point> = HashSet::from_iter(points_in_line);
 
         let line = Line { points: set_of_points };
 
@@ -101,6 +100,8 @@ fn find_line_candidates<'plane>(plane: &'plane Plane, points_in_line: Vec<&'plan
         result.insert(line);
         return result;
     }
+
+    let mut allowed_points = allowed_points;
 
     let point_to_check = points_in_line.last().unwrap();
     for line in &plane.lines {
@@ -129,7 +130,7 @@ fn find_line_candidates<'plane>(plane: &'plane Plane, points_in_line: Vec<&'plan
     result
 }
 
-fn new_plane<'a>(order: u32) -> Plane<'a> {
+fn new_plane<'plane>(order: u32) -> Plane<'plane> {
     let cardinality = order * order + order + 1;
 
     // A finite projective plane of order n has n^2 + n + 1 many points
@@ -145,7 +146,44 @@ fn new_plane<'a>(order: u32) -> Plane<'a> {
 }
 
 fn projective_plane_find(order: u32) {
-    let plane = new_plane(order);
-    let lines = plane.find_all_line_candidates();
-    println!("{:?}", lines)
+    let mut plane = new_plane(order);
+
+    let always_there = create_always_there_lines(order);
+
+    println!("Always there: {:?}", always_there);
+
+    let candidates = plane.find_all_line_candidates();
+}
+
+// TODO: Create some graphics explaining this.
+fn create_always_there_lines(order: u32) -> Vec<Vec<Point>> {
+    let mut always_there_lines = Vec::new();
+
+    let first_line_points: Vec<Point> = (0..order + 1)
+        .map(|x| Point { id: x })
+        .collect();
+    always_there_lines.push(first_line_points);
+
+    for i in 0..order + 1 {
+        let mut line: Vec<Point> = Vec::with_capacity((order + 1) as usize);
+        line.push(Point { id: order + 1 });
+        line.push(Point { id: i });
+        for j in 2..order + 1 {
+            line.push(Point { id: order + (i * (order - 1)) + j })
+        }
+
+        always_there_lines.push(line);
+    }
+
+    for i in order + 2..order + order + 1{
+        let mut line: Vec<Point> = Vec::with_capacity((order + 1) as usize);
+        for j in 0..order + 1 {
+            println!("i = {}, j = {} order = {} ", i, j, order);
+            line.push(Point { id: i + (j * (order - 1)) })
+        }
+
+        always_there_lines.push(line);
+    }
+
+    always_there_lines
 }
