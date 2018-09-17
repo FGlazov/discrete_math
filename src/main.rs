d8722b8f5b20dd6eeead6047d374d2ae9bfb9471
Fedor Glazov <fedorglazov@gmail.com>
2018-09-17 11:44:15 +0200

    Fix bug with orthogonal_to function where it was not using the values inside other.
    Fix usage of value returned by set insert - it was inverted.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,15 @@
 use std::collections::HashSet;
 
-const ORDER: usize = 6;
+const ORDER: usize = 3;
 
 fn main() {
-    let mut state = [[0u16; 6]; 6];
+    let first_entries = [[1, 2, 4], [2, 4, 1], [4, 1, 2]];
+    let second_entries = [[1, 2, 4], [4, 1, 2], [2, 4, 1]];
+
+    let first_latin_square = LatinSquare { entries: first_entries };
+    let second_latin_square = LatinSquare { entries: second_entries };
+
+    println!("{}", first_latin_square.orthogonal_to(second_latin_square));
 }
 
 struct LatinSquare {
@@ -11,17 +17,17 @@ struct LatinSquare {
 }
 
 impl LatinSquare {
-    fn orthogonal_to(&self, other : LatinSquare) -> bool {
+    fn orthogonal_to(&self, other: LatinSquare) -> bool {
         let mut set = HashSet::with_capacity(ORDER * ORDER);
         for i in 0..ORDER {
             for j in 0..ORDER {
                 let first_entry = self.entries[i][j];
-                let second_entry = self.entries[i][j] << 6;
+                let second_entry = other.entries[i][j] << ORDER;
                 let to_put = first_entry ^ second_entry;
-
-                let duplicate = set.insert(to_put);
+                let duplicate = !set.insert(to_put);
                 if duplicate {
-                    return false
+
+                    return false;
                 }
             }
         }
